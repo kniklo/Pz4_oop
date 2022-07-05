15e5408972c9d2fa0b56ed91ccfd5defbc456bde
vvsjunior <vvsjunior@yandex.ru>
2022-07-05 17:10:37 +0300

    !

--- Pz4_oop/B.cpp
+++ Pz4_oop/B.cpp
@@ -1,6 +1,6 @@
 #include "B.h"
 //constructors
-B::B();
+B::B() {};
 B::B(int atr1, int atr2, float atr3, string atr4);
 B::B(const B& from_B);
 B::~B();
@@ -15,9 +15,9 @@ void setatr2(int atr2);
 void setatr3(float atr3);
 void setatr4(string atr4);
 // in out
-friend ostream& operator<< (ostream& out, const B& dot);
-friend istream& operator>> (istream& in, B& dot);
+ostream& operator<< (ostream& out, const B& dot);
+istream& operator>> (istream& in, B& dot);
 // methods
-virtual void m1();
-virtual void m2();
+void m1();
+void m2();
 void m3();
