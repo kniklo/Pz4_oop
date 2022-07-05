0bc1e2ff4856b7f1f6b07b250d08a78b8ecced75
Екатерина Степанова <aniretake2004@gmail.com>
2022-07-05 17:56:34 +0300

    пофиксила ошибки

--- Pz4_oop/D3.cpp
+++ Pz4_oop/D3.cpp
@@ -1,6 +1,8 @@
 #include "D3.h"
 //constructors
 D3::D3() : D1(), D2(), _atr9(0), _atr10(0)
+{
+}
 D3::D3(int atr1, int atr2, float atr3, string atr4, string atr5, float atr6, string atr7, float atr8, int atr9, int atr10) :
 	D1(atr1, atr2, atr3, atr4, atr5, atr6), D2(atr1, atr2, atr3, atr4, atr7, atr8),
 	_atr9(atr9), _atr10(atr10)
@@ -21,12 +23,14 @@ void D3::setatr10(int atr10) { _atr10 = atr10; }
 // in out
 ostream& operator<< (ostream& out, const D3& d3) {
 	out << "atribut9 from D3: " << d3._atr9 << " artibut10 from D3: " << d3._atr10 << "\n";
+	return out;
  }
 istream& operator>> (istream& in, D3& d3) {
 	cout << "Enter  int atribut7 for D3:";
 	cin >> d3._atr9;
 	cout << "Enter int atribut10 for D3:";
 	cin >> d3._atr10;
+	return in;
  }
 //methods
 void D3::m6()
--- Pz4_oop/D4.cpp
+++ Pz4_oop/D4.cpp
@@ -1,6 +1,8 @@
 #include "D4.h"
 //constructors
 D4::D4() : D1(), D2(), _atr11(0), _atr12(0)
+{
+}
 D4::D4(int atr1, int atr2, float atr3, string atr4, string atr5, float atr6, string atr7, float atr8, int atr11, int atr12) :
 	D1(atr1, atr2, atr3, atr4, atr5, atr6), D2(atr1, atr2, atr3, atr4, atr7, atr8),
 	_atr11(atr11), _atr12(atr12)
@@ -22,6 +24,7 @@ void D4::setatr12(int atr12) { _atr12 = atr12; }
 ostream& operator<< (ostream& out, const D4& d4)
 {
 	out << "atribut11 from D4: " << d4._atr11 << " artibut12 from D4: " << d4._atr12 << "\n";
+	return out;
  }
 istream& operator>> (istream& in, D4& d4)
 {
@@ -29,6 +32,7 @@ istream& operator>> (istream& in, D4& d4)
 	cin >> d4._atr11;
 	cout << "Enter int atribut12 for D4:";
 	cin >> d4._atr12;
+	return in;
  }
 //methods
 
